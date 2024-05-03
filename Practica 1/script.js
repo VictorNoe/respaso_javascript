4389d2b50fc5ee9e99f7943579a0ce828b296d13
Victor Noe <20213tn059@utez.edu.mx>
2024-05-02 22:09:54 -0600

    Add practic 1

---
+++ Practica 1/script.js
@@ -0,0 +1,34 @@
+//Declaración de variables
+/*
+const name = "Victor Noe";
+const lasname = "Flores Toledo"
+var age = 21;
+let height = 1.79;
+
+const isMan = true;
+
+if (isMan) {
+    const name = 'Raul'
+    console.log(name, lasname, age, height);
+}
+*/
+
+//Templeante string
+/*
+const name = 'Victor Noe';
+const lasname = 'Flores Toledo';
+const isMan = true;
+console.log(`Nombre completo: ${name} ${lasname} ${isMan ? 'Hombre' : 'Mujer'}`);
+*/
+
+//Arreglos
+const colors = [
+    "Red",
+    "Amarrillo",
+    "Verde",
+    "Azul"
+]
+
+colors.forEach((color) => {
+    console.log(color);
+});
--- script.js
+++
@@ -1,6 +0,0 @@
-const name = "Victor Noe";
-const lasname = "Flores Toledo"
-var age = 21;
-let height = 1.79;
-
-console.log(name, lasname, age, height);
