08554c0f5f82192c3a18c0d35d28e17dd47c89ba
Victor Noe <20213tn059@utez.edu.mx>
2024-05-03 16:59:12 -0600

    Add objet structure

--- Practica 1/script.js
+++ Practica 1/script.js
@@ -22,6 +22,7 @@ console.log(`Nombre completo: ${name} ${lasname} ${isMan ? 'Hombre' : 'Mujer'}`)
 */
 
 //Arreglos
+/*
 const colors = [
     "Red",
     "Amarrillo",
@@ -32,3 +33,70 @@ const colors = [
 colors.forEach((color) => {
     console.log(color);
 });
+*/
+
+//1 persoba puede tener diferentes carros
+//1 persona puede tener diferentes ubicaciones
+//1 persona solo puede tener un campo nombre
+//1 persona solo puede tener apellidos
+//1 persona tiene decha de nacimiento, sexo como datos personales
+//1 persona tiene nacionalidad, pero puede tener más de una por ser naturalizadas (siempre y cuando así sera
+//1 persona puede tener número de cantidad de métodos de pago
+
+
+/*
+carro
+metodo de pago
+ubicaciones
+nacionalidades
+user
+*/
+
+const person = {
+    id: 1,
+    name: 'Victor Noe',
+    lastname: 'Flores Toledo',
+    sex: 'Male',
+    birthday: '24 de noviembre',
+    cars: [
+        {
+            id: 1,
+            name: 'Camaro',
+            price: 512000
+        },
+        {
+            id: 1,
+            name: 'Camaro',
+            price: 512000
+        }
+    ],
+    locations: [
+        {
+            street: 'Emiliano Zapata',
+            state: 'Morelos',
+            latitude: 'wdasdasd123',
+            longitude: 'akjsdnsad'
+        }
+    ],
+    payments: [
+        {
+            type: 'credit card',
+            data: {
+                numberCart: 423412344231234,
+                cvv: 727,
+                dateExpiration: '24/11/2024',
+                propetary: 'Victor Noe Flores Toledo'
+            }
+        }
+    ],
+    nationalities: [
+        {
+            name: 'Mexicano',
+            description: 'Hola'
+        },
+        {
+            name: 'Peruana',
+            description: 'Hola'
+        }
+    ]
+}
